d74f57e82474a37daaf22148e7a9aa4d8ec5e8de
Stephanie Conde <stephaniecondecareer@gmail.com>
2023-02-08 19:22:44 -0800

    commit tree 94adba4af71346cf148ebd9fb0316193ec043c6c

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+ 
   get '/movies/:id/summary', to: 'movies#summary'
   get '/movie_summaries', to: 'movies#summaries'
+  
   resources :directors, only: [:index, :show]
-  resources :reviews, only: [:index]
+  # resources :reviews, only: [:index]
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
